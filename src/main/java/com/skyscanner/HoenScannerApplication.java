7b252178abf6b4e5ba8ccc0b5de5c436c50d6510
czpeng822 <153256821+czpeng822@users.noreply.github.com>
2025-03-06 20:39:45 -0500

    Test success

--- src/main/java/com/skyscanner/HoenScannerApplication.java
+++ src/main/java/com/skyscanner/HoenScannerApplication.java
@@ -4,6 +4,13 @@ import io.dropwizard.core.Application;
 import io.dropwizard.core.setup.Bootstrap;
 import io.dropwizard.core.setup.Environment;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class HoenScannerApplication extends Application<HoenScannerConfiguration> {
 
     public static void main(final String[] args) throws Exception {
@@ -21,7 +28,32 @@ public class HoenScannerApplication extends Application<HoenScannerConfiguration
     }
 
     @Override
-    public void run(final HoenScannerConfiguration configuration, final Environment environment) {
+    public void run(final HoenScannerConfiguration configuration, final Environment environment) throws IOException{
+        ObjectMapper mapper = new ObjectMapper();
+
+        List<SearchResult> carResults = Arrays.asList(
+                mapper.readValue(
+                        getClass().getClassLoader().getResource("rental_cars.json"),
+                        SearchResult[].class
+                )
+        );
+
+        // Read hotel data from the JSON file and map it to a list of SearchResult objects
+        List<SearchResult> hotelResults = Arrays.asList(
+                mapper.readValue(
+                        getClass().getClassLoader().getResource("hotels.json"),
+                        SearchResult[].class
+                )
+        );
+
+        // Create a single list to hold both car and hotel results
+        List<SearchResult> searchResults = new ArrayList<>();
+        searchResults.addAll(carResults);
+        searchResults.addAll(hotelResults);
+
+        // Register the SearchResource with the search results
+        final SearchResource resource = new SearchResource(searchResults);
+        environment.jersey().register(resource);
 
     }
 
