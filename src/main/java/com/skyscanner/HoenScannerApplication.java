fb1942617412789fd42cb180f45fd1aea0998cff
Nidhi Nupur <154660722+nidhi-nupur@users.noreply.github.com>
2025-04-09 15:04:39 +0530

    Update HoenScannerApplication.java

--- src/main/java/com/skyscanner/HoenScannerApplication.java
+++ src/main/java/com/skyscanner/HoenScannerApplication.java
@@ -21,8 +21,26 @@ public class HoenScannerApplication extends Application<HoenScannerConfiguration
     }
 
     @Override
-    public void run(final HoenScannerConfiguration configuration, final Environment environment) {
-
+    public void run(HoenScannerConfiguration configuration, Environment environment) throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        List<SearchResult> searchResults = new ArrayList<>();
+    
+        searchResults.addAll(Arrays.asList(
+            mapper.readValue(
+                Resources.getResource("hotels.json"),
+                SearchResult[].class
+            )
+        ));
+    
+        searchResults.addAll(Arrays.asList(
+            mapper.readValue(
+                Resources.getResource("rental_cars.json"),
+                SearchResult[].class
+            )
+        ));
+    
+        environment.jersey().register(new SearchResource(searchResults));
     }
+
 
 }
